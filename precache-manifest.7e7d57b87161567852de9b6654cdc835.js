17230b6db0c5fea081a01aff2a7f637d28a2b391
GuyK <guyk1994@gmail.com>
2020-10-02 17:50:35 +0300

    Updates

---
+++ precache-manifest.7e7d57b87161567852de9b6654cdc835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65422b6475e54e1d0619d9204d812a18",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8d13b61f12e04abfb474",
+    "url": "/todo-app/static/css/main.cbe197df.chunk.css"
+  },
+  {
+    "revision": "cc0ec2b3e4d73ea9dce7",
+    "url": "/todo-app/static/js/2.13990058.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/todo-app/static/js/2.13990058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d13b61f12e04abfb474",
+    "url": "/todo-app/static/js/main.3f3d7ee7.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
